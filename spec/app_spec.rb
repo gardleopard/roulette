24909b7e210292f18c98af4674ef7b42622d73fb
Gard Rimestad <gardhr@gmail.com>
2011-06-11 22:10:31 +0200

    hmm

---
+++ spec/app_spec.rb
@@ -0,0 +1,23 @@
+require  'app'
+require 'rack/test'
+require 'pp'
+
+
+module MyHelpers
+  def app
+   Sinatra::Application
+  end
+end
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+  conf.include MyHelpers
+end
+describe RouletteService do
+  context "upload" do
+    it "stores an image" do
+      #image = File.open("testimage.jpg") 
+      post '/upload', 'file' => Rack::Test::UploadedFile.new('spec/fixtures/testimage.jpg', 'image/jpg')
+      Dir['uploads/*'].should include('uploads/testimage.jpg') 
+    end
+  end
+end
