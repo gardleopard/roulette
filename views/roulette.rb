4ba4f56294e8497c3474892cd751af4508f5aed9
Gard Rimestad <gardhr@gmail.com>
2011-06-15 07:45:34 +0200

    add link to images so we can register wins and play roulette

--- views/roulette.rb
+++ views/roulette.rb
@@ -1,10 +1,10 @@
 module Views
   class Roulette < Layout
     def image1 
-      "/img/" + @image1['filename']
+      @image1['filename']
     end
     def image2 
-      "/img/" + @image2['filename']
+      @image2['filename']
     end
   end
 end
