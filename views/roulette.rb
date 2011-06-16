965d19dbd909be367912022b9955ecf2aa44e7da
Gard Rimestad <gardhr@gmail.com>
2011-06-16 16:39:57 +0200

    stopped using RMagic to serve images

--- views/roulette.rb
+++ views/roulette.rb
@@ -1,9 +1,15 @@
 module Views
   class Roulette < Layout
     def image1 
-      @image1['filename']
+      @image1['path']+"/"+@image1['filename']
     end
     def image2 
+      @image2['path']+"/"+@image2['filename']
+    end
+    def url1 
+      @image1['filename']
+    end
+    def url2 
       @image2['filename']
     end
   end
